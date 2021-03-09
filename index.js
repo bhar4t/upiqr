4cea80131fafe559b61d7cb6cd4810e8344553b5
bhar4t <seizedbharat@gmail.com>
2021-03-09 11:32:46 +0530

    singleton

--- index.js
+++ index.js
@@ -1,31 +1,27 @@
 const QRCode = require("qrcode");
 
 class UPI {
-     static generateQR({ vpa, pn }) {
-        return new Promise((resolve, reject) => {
-            if (!vpa || !pn) {
-                reject(new Error("Please provide valid Virtual Payee's Address/Payee's Name."));
-            }
-            if ((vpa && vpa.length < 5) || (pn && pn.length < 4)) {
-                reject(new Error("Virtual Payee's Address/Payee's Name is too short."));
-            }
-            const intent = `upi://pay?pa=${vpa}&pn=${pn}`;
-            QRCode.toDataURL(
-                intent,
-                (err, qr) => {
-                  if (err) {
-                    reject(new Error("Unable to generate UPI QR Code."));
-                  }
-                  resolve({
-                        qr,
-                        intent,
-                    })
-                });
-        });
+    constructor() {
+        if (this.upi) return this.upi;
+        this.upi = this;
     }
+    generateQR = ({ vpa, pn }) => new Promise((resolve, reject) => {
+        
+        if (!vpa || !pn) reject(new Error("Please provide valid Virtual Payee's Address/Payee's Name."));
+        if ((vpa && vpa.length < 5) || (pn && pn.length < 4))    reject(new Error("Virtual Payee's Address/Payee's Name is too short."));
+        
+        const intent = `upi://pay?pa=${vpa}&pn=${pn}`;
+        
+        QRCode.toDataURL(
+            intent,
+            (err, qr) => {
+              if (err) reject(new Error("Unable to generate UPI QR Code."));
+              resolve({ qr, intent });
+            });
+    })
 }
 
-module.exports = UPI;
+module.exports = new UPI();
 
 
 // with payment optional

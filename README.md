1977c2652e956991aa076c86e9e353b7fc6df8ff
bhar4t <seizedbharat@gmail.com>
2021-03-10 23:09:26 +0530

    added all fields for intent.

--- README.md
+++ README.md
@@ -3,21 +3,42 @@ Generate NPCI's UPI QR code (BASE64) along with UPI intent link, , By using it a
 This package will work on client and server.
 
 ```js
-  const UPI =  require('upiqr');
 
-  UPI
-  .generateQR({
-    vpa: 'john@upi',
-    pn: "JOHN DOE"
+  const upiqr =  require('upiqr');
+
+  upiqr
+  .generate({
+    payeeVPA: 'john@upi',
+    payeeName: "JOHN DOE"
   })
   .then((upi) => {
-    console.log(upi.qr);      // Base64 URL
+    console.log(upi.qr);      // data:image/png;base64,eR0lGODP...
     console.log(upi.intent);  // upi://pay?pa=john@upi&pn=JOHN DOE
   })
   .catch(err => {
     console.log(err);
   });
 
+
 ```
+
+### Fields detail:
+
+| Fields              | Description                                                | Default  | Required  |
+|---------------------|------------------------------------------------------------|----------|-----------|
+| payeeVPA            | VPA address from UPI payment account                       |          | Mandatory | pa
+| payeeName           | Merchant Name registered in UPI payment account            |          | Mandatory | pn
+| payeeMerchantCode   | Merchant Code from UPI payment account                     |          | Optional  | me
+| transactionId       | Unique transaction id for merchant's reference             |          | Optional  | tid
+| transactionRef      | Unique transaction id for merchant's reference             |          | Optional  | tr
+| transactionNote     | Note that will displayed in payment app during transaction |          | Optional  | tn
+| amount              | Amount                                                     |          | Optional  | am
+| minimumAmount       | Minimum amount that has to be transferred                  |          | Optional  | mam
+| currency            | Currency of amount                                         | INR      | Optional  | cu
+| transactionRefUrl   | URL for the order                                          |          | Optional  | url
+
+In table, fields requirement column is based on static QR, For dynamic QR you need to change more fields along with `payeeVPA` and `payeeName`.
+
+For a complete list of supported fields, refer to the [NPCI UPI Linking Specs](https://www.npci.org.in/PDF/npci/upi/circular/2017/Circular18_BankCompliances_to_enbaleUPIMerchantecosystem_0.pdf)
 
 Internally using `qrcode` for QR Generation.
--- index.js
+++ index.js
@@ -1,35 +1,64 @@
 const QRCode = require("qrcode");
 
+function validateParams({ pa, pn }) {
+    if (!pa || !pn) {
+        return "Please provide valid Virtual Payee's Address/Payee's Name.";
+    } else if ((pa && pa.length < 5) || (pn && pn.length < 4)) {
+        return "Virtual Payee's Address/Payee's Name is too short."   
+    } else return false;
+    // tr: Transaction Ref No. (upto 35 digits) 
+}
+
+function buildUrl(url, parameters) {
+    var qs = "";
+    for(var key in parameters) {
+      var value = parameters[key];
+      qs += encodeURIComponent(key) + "=" + encodeURIComponent(value) + "&";
+    }
+    if (qs.length > 0) url = url + qs;
+    return url;
+  }
+  
 class UPI {
     constructor() {
         if (this.upi) return this.upi;
         this.upi = this;
     }
-    generateQR = ({ vpa, pn }) => new Promise((resolve, reject) => {
-        
-        if (!vpa || !pn) reject(new Error("Please provide valid Virtual Payee's Address/Payee's Name."));
-        if ((vpa && vpa.length < 5) || (pn && pn.length < 4))    reject(new Error("Virtual Payee's Address/Payee's Name is too short."));
-        
-        const intent = `upi://pay?pa=${vpa}&pn=${pn}`;
+    generate = ({
+        payeeVPA: pa,
+        payeeName: pn,
+        payeeMerchantCode: me,
+        transactionId: tid,
+        transactionRef: tr,
+        transactionNote: tn,
+        amount: am,
+        minimumAmount: mam,
+        currency: cu,
+        transactionRefUrl: url,
+    }) => new Promise((resolve, reject) => {
+
+        let error = validateParams({ pa, pn });
+        if (error) reject(new Error(error));
+
+        let intent = "upi://pay?";
+        if (pa) intent = buildUrl(intent, { pa, pn });
+        if (am) intent = buildUrl(intent, { am });
+        if (mam) intent = buildUrl(intent, { mam });
+        if (cu) intent = buildUrl(intent, { cu });
+        if (me) intent = buildUrl(intent, { me });
+        if (tid) intent = buildUrl(intent, { tid });
+        if (tr) intent = buildUrl(intent, { tr });
+        if (tn) intent = buildUrl(intent, { tn });
+        intent = intent.substring(0, intent.length-1);
         
         QRCode.toDataURL(
             intent,
             (err, qr) => {
               if (err) reject(new Error("Unable to generate UPI QR Code."));
               resolve({ qr, intent });
-            });
+            }
+        );
     })
 }
 
 module.exports = new UPI();
-
-
-// with payment optional
-// am=696&mode=01
-
-// without payment
-// mode=01
-
-
-// bhim
-// cu=INR&mode=02&purpose=00

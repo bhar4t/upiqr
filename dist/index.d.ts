77084cd2729c2b61bb36eb4fc126c387f1a83a34
BHAR4T <bhar4t@outlook.com>
2023-08-27 11:54:49 +0530

    revert actions

---
+++ dist/index.d.ts
@@ -0,0 +1,2 @@
+import { UPIIntentParams, UPIIntentResult } from './types/upiqr';
+export default function upiqr({ payeeVPA: pa, payeeName: pn, payeeMerchantCode: me, transactionId: tid, transactionRef: tr, transactionNote: tn, amount: am, minimumAmount: mam, currency: cu, }: UPIIntentParams): Promise<UPIIntentResult>;
---
+++ dist/utils/helper.d.ts
@@ -0,0 +1,5 @@
+export declare function validateParams({ pa, pn }: {
+    pa: string | undefined;
+    pn: string | undefined;
+}): string;
+export declare function buildUrl(this: string, params: object): string;

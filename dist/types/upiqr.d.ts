a9f38f1d49f9336a66a3b31551778a9899423e12
bhar4t <seizedbharat@gmail.com>
2024-12-31 20:03:40 +0530

    Added jsmin minify

--- dist/types/upiqr.d.ts
+++ dist/types/upiqr.d.ts
@@ -1,31 +1,3 @@
-/**
- * Supported image types for the QR code.
- */
-export type ImageType = 'png' | 'jpeg' | 'webp';
-/**
- * Base64 encoded image string.
- * @template imageType - The type of the image.
- */
-export type Base64<imageType extends ImageType> = `data:image/${imageType};base64,${string}`;
-/**
- * Parameters for generating UPI intent.
- */
-export interface UPIIntentParams {
-    payeeVPA: string;
-    payeeName: string;
-    payeeMerchantCode?: string;
-    transactionId?: string;
-    transactionRef?: string;
-    transactionNote?: string;
-    amount?: string;
-    minimumAmount?: string;
-    currency?: string;
-    transactionRefUrl?: string;
-}
-/**
- * Result of the UPI QR code generation.
- */
-export interface QRResult {
-    qr: Base64<ImageType>;
-    intent: string;
-}
+
+export type ImageType='png'|'jpeg'|'webp';export type Base64<imageType extends ImageType>=`data:image/${imageType};base64,${string}`;export interface UPIIntentParams{payeeVPA:string;payeeName:string;payeeMerchantCode?:string;transactionId?:string;transactionRef?:string;transactionNote?:string;amount?:string;minimumAmount?:string;currency?:string;transactionRefUrl?:string;}
+export interface QRResult{qr:Base64<ImageType>;intent:string;}
